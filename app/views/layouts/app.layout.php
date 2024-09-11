8fbea1347851908cf63cd821996213ef89e2e9ff
JuanSebastianBG <juansebastianbernalgamboa@gmail.com>
2024-09-11 11:42:39 -0500

    feat: create app.layout.php, footer.php, header.php AND move home.view.php to pages

---
+++ app/views/layouts/app.layout.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es" data-theme="light">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio | Universidad</title>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="flex flex-col min-h-screen">
+    <?php require_once(__DIR__ . '/header.php') ?>
+
+    <main class="grow">
+        
+    </main>
+
+    <?php require_once(__DIR__ . '/footer.php') ?>
+</body>
+
+</html>
